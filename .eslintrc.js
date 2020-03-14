3fcb56fcc1f540bfbfd44f24c43a5240d1b45a66
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-03-14 18:12:45 +0100

    add estlint and update jest configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-  extends: ['airbnb-base'],
+  root: true,
+  extends: ['airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+  },
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
--- __test__/App.spec.jsx
+++ __test__/App.spec.jsx
@@ -1,8 +1,7 @@
 import 'react-native';
 import React from 'react';
-import App from './App';
-
 import { create } from 'react-test-renderer';
+import App from '../App';
 
 it('renders correctly', () => {
   const tree = create(() => <App />).toJSON();
