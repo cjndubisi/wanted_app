96df5134bc7e15ca1e13e1482ef39cc729e3ca8d
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-03-14 17:00:56 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
