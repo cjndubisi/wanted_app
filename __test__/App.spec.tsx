24732ad69a8855a76324b9d23871e2e35a19be11
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-03-14 18:41:02 +0100

    fix warning when using <App /> in App.spec.js

--- __test__/App.spec.tsx
+++ __test__/App.spec.tsx
@@ -4,7 +4,7 @@ import { create } from 'react-test-renderer';
 import App from '../App';
 
 it('renders correctly', () => {
-  const tree = create(() => <App />).toJSON();
+  const tree = create(<App />).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
