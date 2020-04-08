9b2b574d91cf9ea19bd978dab2db03784eaeabab
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-04-08 20:55:57 +0100

    feat: embed in packages

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -2,6 +2,9 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['babel-plugin-styled-components'],
+    plugins: [
+      'babel-plugin-styled-components',
+      "@babel/plugin-proposal-export-default-from"
+    ],
   };
 };
