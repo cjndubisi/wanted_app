3662634c00504d786e0ee44ab2fcdbacbd193c48
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-04-11 12:14:24 +0100

    monorepo

--- packages/web/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('wanted', () => App);
-
-const rootTag = document.getElementById('root') || document.getElementById('main');
-AppRegistry.runApplication('Wanted', { rootTag });
